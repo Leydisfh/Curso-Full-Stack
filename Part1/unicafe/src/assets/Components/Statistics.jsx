7562547b1abb5f3fc27272a8a84a811ae63501e8
Leydis <leydisfh@gmail.com>
2024-03-21 16:02:58 -0500

    1.11/ unicafe

--- Part1/unicafe/src/assets/Components/Statistics.jsx
+++ Part1/unicafe/src/assets/Components/Statistics.jsx
@@ -2,14 +2,14 @@ import StatisticLine from "./StatisticLine"
 
 const Statistics = ({good, neutral,bad,total,average, positive }) => {
     return (
-        <div>
+        <table>
             <StatisticLine  text={'Good'} value={good}/>
             <StatisticLine  text={'Neutral'} value={neutral}/>
             <StatisticLine  text={'Bad'} value={bad}/>
             <StatisticLine  text={'All'} value={total}/>
             <StatisticLine  text={'Averange'} value={average}/>
             <StatisticLine  text={'Positive'} value={positive + '%'}/>
-        </div>
+        </table>
     )
 }
 

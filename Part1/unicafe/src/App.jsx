8c52a541458959c8ad56e20e9bb92f4e7a7fbba6
Leydis <leydisfh@gmail.com>
2024-03-21 11:44:09 -0500

    1.6 / unicafe

--- Part1/unicafe/src/App.jsx
+++ Part1/unicafe/src/App.jsx
@@ -1,11 +1,24 @@
-
-function App() {
+import  { useState } from 'react';
+import Buttons from "./assets/Components/Buttons"
+const App = () => {
+  // guarda los clics de cada botón en su propio estado
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0) ;
+  
 
 
   return (
-    <>
-     
-    </>
+    <div>
+     <h2>Give feedback</h2>
+      <Buttons name={'good'} onClick={()=> setGood ( good + 1) } />
+      <Buttons name={'neutral'}  onClick={()=> setNeutral (neutral + 1)} />
+      <Buttons name={'bad'}  onClick={()=> setBad (bad + 1)}/>
+      <h2>Statistics</h2>
+      <p>Good: { good }</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+    </div>
   )
 }
 

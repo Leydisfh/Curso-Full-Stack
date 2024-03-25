948e11873caf175a374ed5cb4725777a9235d86d
Leydis <leydisfh@gmail.com>
2024-03-25 16:43:06 -0500

    2.6 / phoneBook

--- Part2/phoneBook/src/App.jsx
+++ Part2/phoneBook/src/App.jsx
@@ -6,18 +6,43 @@ function App() {
   ]) 
   const [newName, setNewName] = useState('')
 
+
+// Funciones
+  const handleChange = (e) => {
+    setNewName(e.target.value)
+  }
+
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    const newPerson = {
+          name: newName
+    }
+    setPersons([...persons, newPerson])
+    setNewName('');
+  }
+
   return (
     <>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
-          name: <input />
+          name: <input type='text' 
+                value={newName}
+                onChange={handleChange} 
+                />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
-      <h2>Numbers</h2> 
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map(person =>{
+          return <li key={person.name}>
+                      {person.name}
+                  </li>
+        })}
+      </ul>
     </>
   )
 }

49e931ccfe3b2072610e7c9da9cbd38d1a802032
Leydis <leydisfh@gmail.com>
2024-03-25 16:25:30 -0500

    Iniciando repo

---
+++ Part2/phoneBook/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+
+function App() {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  return (
+    <>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2> 
+    </>
+  )
+}
+
+export default App

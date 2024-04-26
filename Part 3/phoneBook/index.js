7c7d5b16d8e9e800883529b07a8eaf5caa724eb5
Leydis <leydisfh@gmail.com>
2024-04-26 14:47:55 -0500

    3.2 phoneBook

--- Part 3/phoneBook/index.js
+++ Part 3/phoneBook/index.js
@@ -31,6 +31,10 @@ app.get('/', (req, res) => {
 app.get('/api/persons', (req, res)=>{
     res.json(persons)
 })
+app.get('/info', (req, res)=>{
+  const date = new Date()
+  res.send(`<p>Phonebook has info for ${persons.length} people</p> ` + date)
+})
 
 const port = 3001
 app.listen(port, () => {
